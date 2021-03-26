c1b92189a567cd3836bc5d4ef325460ba3ed10c8
Yordan Stayokv <dark.mat73r@gmail.com>
2021-03-26 11:20:31 +0200

    Starting over

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 require('./config/express')(app);
-require('./config/database')();
+require('./config/mongoose')();
 
 const routes = require('./routes');
 app.use(routes);
