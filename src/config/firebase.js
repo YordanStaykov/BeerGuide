bf86045581cc7ddd076400e59eda9273fcce567e
Yordan Stayokv <dark.mat73r@gmail.com>
2021-03-27 14:38:50 +0200

    Cleaned the boiler-plate code and set Firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyB6B9i5L85WxIpPZGourCygxxKnugzKq8g",
+    authDomain: "beer-receipt-builder.firebaseapp.com",
+    projectId: "beer-receipt-builder",
+    storageBucket: "beer-receipt-builder.appspot.com",
+    messagingSenderId: "366501364826",
+    appId: "1:366501364826:web:bc45a161103ad051c78463"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
