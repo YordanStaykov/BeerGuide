03310cc044561470d4a1d1c6a6e5b3b0cd372f6b
Yordan Stayokv <dark.mat73r@gmail.com>
2021-03-27 20:37:01 +0200

    Added a logo

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,27 +1,59 @@
 import style from './Register.module.scss';
+
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
+
+import { auth } from '../../config/firebase'
 
 const Register = (props) => {
-    function something(e) {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [repeatPassword, setRePassword] = useState('');
+
+    function emailChangeHandler(e) {
+        setEmail(e.target.value)
+    }
+
+    function passwordChangeHandler(e) {
+        setPassword(e.target.value)
+    }
+
+    function repeatPasswordChangeHandler(e) {
+        setRePassword(e.target.value)
+    }
+
+    function submitHandler(e) {
         e.preventDefault()
-        console.log('something');
+
+        if (password !== repeatPassword) {
+            console.log('Something went wrong');
+            return;
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .then(user => {
+                console.log(user);
+            })
+            .catch(err => console.log(err));
+
+
     }
 
     return (
-        <form onSubmit={something}>
+        <form onSubmit={submitHandler}>
             <div className={style.container}>
                 <h1>Register</h1>
                 <p>Please fill in this form to create an account.</p>
                 <hr />
 
                 <label htmlFor="email"><b>Email</b></label>
-                <input type="text" placeholder="Enter Email" name="email" id="email" required />
+                <input type="text" placeholder="Enter Email" name="email" id="email" required onChange={emailChangeHandler} />
 
                 <label htmlFor="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" id="psw" required />
+                <input type="password" placeholder="Enter Password" name="psw" id="psw" required onChange={passwordChangeHandler} />
 
                 <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
-                <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required />
+                <input type="password" placeholder="Repeat Password" name="psw-repeat" id="psw-repeat" required onChange={repeatPasswordChangeHandler} />
                 <hr />
 
                 <p>By creating an account you agree to our <a href="/">Terms & Privacy</a>.</p>
