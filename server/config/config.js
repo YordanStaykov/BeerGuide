a426c13f667f5020bc82da3c0bdc7ca83635bb05
Yordan Stayokv <dark.mat73r@gmail.com>
2021-03-27 13:08:18 +0200

    starting over

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    PORT: 5000,
-    DB_CONNECTION: 'mongodb://localhost/beer-shop',
-    SALT_ROUNDS: 11,
-    SECRET: 'MyLittleSecret',
-    COOKIE_NAME: 'USER_SESSION',
-};
-
-module.exports = config;
