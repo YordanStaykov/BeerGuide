8313ed01914403e7a3925ab23fcea14ed1791101
Yordan Stayokv <dark.mat73r@gmail.com>
2021-07-26 13:19:53 +0300

    Update Recipes.jsx

--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,41 +1,45 @@
-import style from './Recipes.module.scss';
+import style from "./Recipes.module.scss";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import * as recipesService from '../../services/recipes';
+import * as recipesService from "../../services/recipes";
 
-import RecipeCard from './RecipeCard/RecipeCard';
+import RecipeCard from "./RecipeCard/RecipeCard";
 
 const Recipes = () => {
-    const [recipes, setRecipes] = useState([]);
+	const [recipes, setRecipes] = useState([]);
 
-    useEffect(() => {
-        recipesService.getAll()
-            .then(recipes => setRecipes(recipes))
-            .catch(err => console.log(err))
-    }, []);
+	useEffect(() => {
+		recipesService
+			.getAll()
+			.then((recipes) => setRecipes(recipes))
+			.catch((err) => console.log(err));
+	}, []);
 
-    return (
-        <section>
-            <div className={style.addedRecipes}>
-                {
-                    recipes.length > 0 ?
-                        (
-                            (recipes.map(({
-                                style,
-                                _id,
-                                imageURL,
-                                name,
-                            }) => {
-                                return <RecipeCard key={_id} _id={_id} style={style} imageURL={imageURL} name={name} />
-                            }))
-                        )
-                        : <h1>No recipes to show at the moment. Feel free to add one :)</h1>
-                }
-            </div>
-
-        </section>
-    );
+	return (
+		<section>
+			<div className={style.addedRecipes}>
+				{recipes.length > 0 ? (
+					recipes.map(({ style, _id, imageURL, name }) => {
+						return (
+							<RecipeCard
+								key={_id}
+								_id={_id}
+								style={style}
+								imageURL={imageURL}
+								name={name}
+							/>
+						);
+					})
+				) : (
+					<h1>
+						No recipes to show at the moment. Feel free to add one
+						:)
+					</h1>
+				)}
+			</div>
+		</section>
+	);
 };
 
 export default Recipes;
